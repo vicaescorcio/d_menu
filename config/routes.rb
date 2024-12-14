990dafbb83943fe286bfff7f3a06a61871125b84
Vicente Escorcio <vica.escorcio@gmail.com>
2024-12-14 17:58:24 +0100

    Migrate react-rails to react-on-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   get 'home/index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
