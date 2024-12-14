f28911062afc7a45d3492b4912f278acf1e71669
Vicente Escorcio <vica.escorcio@gmail.com>
2024-12-14 11:24:17 +0100

    Add typescript dependency

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module DMenu
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.react.server_renderer_extensions = ["jsx", "js", "tsx", "ts"]
   end
 end
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,10 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+const { merge } = require("webpack-merge");
+const { generateWebpackConfig } = require("shakapacker");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-const webpackConfig = generateWebpackConfig()
+const webpackConfig = generateWebpackConfig();
 
-module.exports = webpackConfig
+module.exports = merge(webpackConfig, {
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+});
