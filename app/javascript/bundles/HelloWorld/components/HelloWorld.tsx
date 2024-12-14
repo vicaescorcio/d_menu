f9549851bb81c0ff1542bc346083d49a3176bcb0
Vicente Escorcio <vica.escorcio@gmail.com>
2024-12-14 19:11:27 +0100

    Add material ui react

--- app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
@@ -1,13 +1,22 @@
 import PropTypes from "prop-types";
 import React, { useState } from "react";
-import style from "./HelloWorld.module.css";
+import * as style from "./HelloWorld.module.css";
 import Test from "./Test";
+import { Typography } from "@mui/material";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const HelloWorld = (props) => {
   const [name, setName] = useState(props.name);
 
   return (
     <div>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Material-UI
+      </Typography>
       <h3>Hello, {name}!</h3>
       <hr />
       <Test />
